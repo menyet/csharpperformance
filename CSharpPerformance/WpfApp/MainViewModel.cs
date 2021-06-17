21b4c7ab45371d5d464f3380c83edf5d75c358bf
Andrej Mandli <andrej.mandli@betterask.erni>
2021-06-17 15:40:21 +0200

    Cpu-, IO-bound and locking ops

--- CSharpPerformance/WpfApp/MainViewModel.cs
+++ CSharpPerformance/WpfApp/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
@@ -9,9 +10,16 @@ namespace WpfApp
 {
     public class MainViewModel : ObservableObject
     {
+        private readonly ISomeService _service;
+
         public MainViewModel(ISomeService service)
         {
-            Command = new RelayCommand(() => service.Action().Wait());
+            _service = service;
+            DeadlockCommand = new RelayCommand(LockingOperation);
+
+            CpuBoundCommand = new RelayCommand(CpuBoundOperation);
+
+            IoBoundCommand = new RelayCommand(IoBoundOperation);
 
             Command2 = new RelayCommand(() =>
             {
@@ -22,7 +30,39 @@ namespace WpfApp
 
         public ICommand Command2 { get; }
 
-        public ICommand Command { get; }
+        public ICommand DeadlockCommand { get; }
+
+        public ICommand CpuBoundCommand { get; }
+
+        public ICommand IoBoundCommand { get; }
+
+        private void LockingOperation()
+        {
+            _service.Action().Wait();
+        }
+
+        private void CpuBoundOperation()
+        {
+            static int Foo(int l)
+            {
+                if (l == 0)
+                {
+                    return 1;
+                }
+                return Math.Max(Foo(l - 1), Foo(l - 1)) + 1;
+            }
+
+            Stopwatch w = new Stopwatch();
+            w.Start();
+            var result = Foo(30);
+            w.Stop();
+            MessageBox.Show($"Foo returned {result} in {w.ElapsedMilliseconds} ms");
+        }
+
+        private void IoBoundOperation()
+        {
+            _service.ReadData().Wait();
+        }
 
     }
 
